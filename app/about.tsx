c6c6962eb365a4a4eb13db8b6d69d5a547e30818
Mayank Rawat <mayank.rawat6802@gmail.com>
2024-10-30 10:03:07 +0530

    update about section

--- app/about.tsx
+++ app/about.tsx
@@ -1,13 +1,13 @@
 export default function About() {
     return (
         <div>
-            <p>
+            <p className="whitespace-pre-wrap">
             Hi, I am Mayank Rawat and I am a computer science graduate. I love
         to build and break stuff using code.{"\n"}
         I have been a part of the E-Commerce team at Fastenal where 
         I was primarily responsible for managing the existing 
-        e-commerce website and building new features.{"\n"}
-        I have got work experience in{" "}
+        e-commerce website and building new features. I have got work 
+        experience in{" "}
         <span className="text-white font-semibold">Java, Spring Boot, React, 
         and Microservices.</span>{"\n"}
         Further, I have hands-on experience in{" "}
